67ce9e33909a8a977782830066eb462213a171c3
ardeshirsaadat <ardeshirsaadat@gmail.com>
2022-04-25 13:11:12 -0400

    insert rows into all tables

--- sql_queries.py
+++ sql_queries.py
@@ -10,7 +10,7 @@ time_table_drop = "DROP table IF EXISTS time"
 
 songplay_table_create = ("""
                             CREATE TABLE IF NOT EXISTS songplays
-                            (songplay_id int PRIMARY KEY,start_time timestamp,user_id int,level text,song_id int,artist_id int,session_id int,location text,user_agent text);
+                            (songplay_id SERIAL PRIMARY KEY,start_time timestamp,user_id int,level text,song_id int,artist_id int,session_id int,location text,user_agent text);
 """)
 
 user_table_create = ("""
@@ -36,9 +36,15 @@ time_table_create = ("""
 # INSERT RECORDS
 
 songplay_table_insert = ("""
+                            INSERT INTO songplays (start_time,user_id,level,song_id,artist_id,session_id,location,user_agent)
+                            VALUES (%s,%s,%s,%s,%s,%s,%s,%s)
 """)
 
 user_table_insert = ("""
+                        INSERT INTO users (user_id,first_name,last_name,gender,level)
+                        VALUES (%s,%s,%s,%s,%s)
+                        ON CONFLICT (user_id) 
+                        DO NOTHING;
 """)
 
 song_table_insert = ("""
@@ -53,11 +59,16 @@ artist_table_insert = ("""
 
 
 time_table_insert = ("""
+                        INSERT INTO time (start_time,hour,day,week,month,year,weekday)
+                        VALUES (%s,%s,%s,%s,%s,%s,%s)
 """)
 
 # FIND SONGS
 
 song_select = ("""
+                  SELECT songs.song_id, songs.artist_id FROM songs
+                  JOIN artists ON 
+                  ((songs.title = %s) AND (artists.name = %s) AND (songs.duration = %s));
 """)
 
 # QUERY LISTS
